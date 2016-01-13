b19ed590184ef146ff7520ea6cd42b06aca0411b
Andrew <burkhart.andrew.j@gmail.com>
2016-01-13 12:54:18 -0500

    Revised validity method for operator. Inserted into operator case statement.

--- 101_programming_foundations/lesson_2/calculator.rb
+++ 101_programming_foundations/lesson_2/calculator.rb
@@ -8,11 +8,11 @@
 # defining methods
 
 def prompt(message)
-  puts("=> #{message}")
+  puts "=> #{message}"
 end
 
 def valid_number?(num)
-  num.to_i() != 0
+  num.to_i != 0
 end
 
 def operation_to_message(operator)
@@ -29,40 +29,40 @@ def operation_to_message(operator)
 end
 
 # Welcome Prompt and user name
-prompt("Welcome to Calculator! Please enter your name:")
+prompt "Welcome to Calculator! Please enter your name:"
 name = gets.chomp
 
 loop do
-  if name.empty?()
-    prompt('Make sure to enter a valid name.')
+  if name.empty?
+    prompt 'Make sure to enter a valid name.'
   else
     break
   end
 end
 
-prompt("Hello #{name}! Lets tackle those calculations now.")
+prompt "Hello #{name}! Lets tackle those calculations now."
 
 loop do # Main Loop
   # collecting user numbers
   number_1 = ''
   loop do
-    prompt('Please enter the first number:')
+    prompt 'Please enter the first number:'
     number_1 = gets.chomp.to_i
     if valid_number?(number_1)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number")
+      prompt "Hmm... that doesn't look like a valid number"
     end
   end
 
   number_2 = ''
   loop do
-    prompt("Please enter the second number:")
+    prompt "Please enter the second number:"
     number_2 = gets.chomp.to_i
     if valid_number?(number_2)
       break
     else
-      prompt("Hmm... that doesn't look like a valid number")
+      prompt "Hmm... that doesn't look like a valid number"
     end
   end
 
@@ -76,56 +76,40 @@ loop do # Main Loop
   4) divide
   MSG
 
-  prompt(operator_prompt)
-
-  operator = ''
-  loop do
-    operator = gets.chomp
-
-    if %w(1 2 3 4).include?(operator)
-      break
-    else
-      prompt("#{name} that's not a valid operator. Please choose 1, 2, 3, 4.")
-    end
-  end
-
-  prompt("#{operation_to_message(operator)} #{number_1} and #{number_2}...")
-
-  # operator specific calculations
-
-  # add
-  # if operator == "1"
-  #   result = number_1 + number_2
-
-  # #subtract
-  # elsif operator == "2"
-  #   result = number_1 - number_2
-
-  # #multiply
-  # elsif operator == "3"
-  #   result = number_1 * number_2
-  # #divide
-  # else operator == "4"
-  #   result = number_1.to_f / number_2.to_f
+  prompt operator_prompt
 
   # refactored operator calculations
-  result = case operator
-           when '1'
-             number_1 + number_2
-           when '2'
-             number_1 - number_2
-           when '3'
-             number_1 * number_2
-           when '4'
-             number_1.to_f / number_2.to_f
-           end
+  operator = ''
+  result = ''
+  loop do
+    operator = gets.chomp
+    result = case operator
+             when '1'
+               result = number_1 + number_2
+               break
+             when '2'
+               result = number_1 - number_2
+               break
+             when '3'
+               result = number_1 * number_2
+               break
+             when '4'
+               result = number_1.to_f / number_2.to_f
+               break
+             else
+               prompt "#{name} that's not a valid operator. Please choose 1, 2, 3, 4."
+             end
+  end
+
+  # summary of operation and numbers
+  prompt "#{operation_to_message(operator)} #{number_1} and #{number_2}..."
 
   # output the result to the user
-  prompt("Results in #{result}")
+  prompt "Results in #{result}"
 
-  prompt("Woah... that was an amazing calculation you did #{name}. Type Y to perform another.")
+  prompt "Woah... that was an amazing calculation you did #{name}. Type Y to perform another."
   answer = gets.chomp
-  break unless answer.downcase().start_with?('y')
+  break unless answer.downcase.start_with?('y')
 end # end of Main Loop
 
-prompt('Thank you for using Calculator. Good bye!')
+prompt 'Thank you for using Calculator. Good bye!'
