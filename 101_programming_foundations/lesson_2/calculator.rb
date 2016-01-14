9e8d4cb7dcb3a73a628416b4d749276ca7ead8da
Andrew <burkhart.andrew.j@gmail.com>
2016-01-13 21:42:22 -0500

    Improved number validity methods and other refactoring.

--- 101_programming_foundations/lesson_2/calculator.rb
+++ 101_programming_foundations/lesson_2/calculator.rb
@@ -5,16 +5,90 @@
 # ask for the type of operation to perform: add, subtract, mulitply or divide
 # displays a result
 
-# defining methods
+###################
+# METHODS
+###################
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_number?(num)
-  num.to_i != 0
+# collecting user name
+def get_name()
+  prompt "Welcome to Calculator! Please enter your name:"
+  name = gets.chomp
+  loop do
+    if name.empty?
+      prompt 'Make sure to enter a valid name.'
+    else
+      break
+    end
+  end
+  return name
 end
 
+# number validations
+def number?(input)
+  integer?(input) || float?(input)
+end
+def integer?(input)
+  /^\d+$/.match(input)
+end
+def float?(input)
+  /\d/.match(input) && /^\d*\.?\d*$/.match(input)
+end
+
+# collecting user number input
+def collect_num(instance)
+  loop do
+  prompt "Please enter the #{instance} number:"
+  number = gets.chomp
+    if number?(number) == nil
+      prompt "Hmm... that doesn't look like a valid number"
+    else
+      return number.to_f
+      break
+    end
+  end
+end
+
+# collecting user operator selection
+def get_operator()
+  operator_prompt = <<-MSG
+  Please enter the number of the operator you would like to perform:
+  1) add
+  2) subtract
+  3) multiply
+  4) divide
+  MSG
+  prompt operator_prompt
+  loop do
+    operator = gets.chomp
+      if /[1-4]/.match(operator) == nil
+        prompt "#{name} that's not a valid operator. Please choose 1, 2, 3, 4."
+      else
+        return operator
+        break
+      end
+  end
+end
+
+# performing calculation using user operator and number selections
+def calculate(operator, number_1, number_2)
+  case operator
+  when '1'
+    result = number_1 + number_2
+  when '2'
+    result = number_1 - number_2
+  when '3'
+    result = number_1 * number_2
+  when '4'
+    result = number_1.to_f / number_2.to_f
+  end
+  return result
+end
+
+# displays selected operator
 def operation_to_message(operator)
   case operator
   when '1'
@@ -28,88 +102,38 @@ def operation_to_message(operator)
   end
 end
 
-# Welcome Prompt and user name
-prompt "Welcome to Calculator! Please enter your name:"
-name = gets.chomp
+###################
+# ENTER THE CALCULATOR
+###################
 
-loop do
-  if name.empty?
-    prompt 'Make sure to enter a valid name.'
-  else
-    break
-  end
-end
+# collecting user name
+name = get_name()
 
+# welcome prompt
 prompt "Hello #{name}! Lets tackle those calculations now."
 
-loop do # Main Loop
+# main loop
+loop do
   # collecting user numbers
   number_1 = ''
-  loop do
-    prompt 'Please enter the first number:'
-    number_1 = gets.chomp.to_i
-    if valid_number?(number_1)
-      break
-    else
-      prompt "Hmm... that doesn't look like a valid number"
-    end
-  end
-
+  number_1 = collect_num("first")
   number_2 = ''
-  loop do
-    prompt "Please enter the second number:"
-    number_2 = gets.chomp.to_i
-    if valid_number?(number_2)
-      break
-    else
-      prompt "Hmm... that doesn't look like a valid number"
-    end
-  end
+  number_2 = collect_num("second")
 
-  # asking for operator
-
-  operator_prompt = <<-MSG
-  Please enter the number of the operator you would like to perform:
-  1) add
-  2) subtract
-  3) multiply
-  4) divide
-  MSG
-
-  prompt operator_prompt
-
-  # refactored operator calculations
-  operator = ''
-  result = ''
-  loop do
-    operator = gets.chomp
-    result = case operator
-             when '1'
-               result = number_1 + number_2
-               break
-             when '2'
-               result = number_1 - number_2
-               break
-             when '3'
-               result = number_1 * number_2
-               break
-             when '4'
-               result = number_1.to_f / number_2.to_f
-               break
-             else
-               prompt "#{name} that's not a valid operator. Please choose 1, 2, 3, 4."
-             end
-  end
+ # obtaining user selected operator
+ operator = get_operator
+ result = calculate(operator, number_1, number_2)
 
   # summary of operation and numbers
   prompt "#{operation_to_message(operator)} #{number_1} and #{number_2}..."
 
   # output the result to the user
   prompt "Results in #{result}"
-
   prompt "Woah... that was an amazing calculation you did #{name}. Type Y to perform another."
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
-end # end of Main Loop
+end 
+# end main loop
 
+# good bye prompt
 prompt 'Thank you for using Calculator. Good bye!'
