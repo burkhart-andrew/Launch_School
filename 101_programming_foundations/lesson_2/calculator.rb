2bac9181d86e76f968e7d3ddd53c62cf75337111
Andrew <burkhart.andrew.j@gmail.com>
2016-01-13 23:23:40 -0500

    Fixed rubocop offenses. Fixed collect name method logic.

--- 101_programming_foundations/lesson_2/calculator.rb
+++ 101_programming_foundations/lesson_2/calculator.rb
@@ -14,41 +14,41 @@ def prompt(message)
 end
 
 # collecting user name
-def get_name()
+def collect_name
   prompt 'Welcome to Calculator! Please enter your name:'
   name = gets.chomp
   loop do
     if name.empty?
       prompt 'Make sure to enter a valid name.'
     else
-      break
+      return name
     end
   end
-  return name
 end
 
 # number validations
 def number?(input)
   integer?(input) || float?(input)
 end
+
 def integer?(input)
   /^\d+$/.match(input)
 end
+
 def float?(input)
   /\d/.match(input) && /^\d*\.?\d*$/.match(input)
 end
 
 # collecting user number input
-def get_user_input(p)
+def user_input(p)
   loop do
     prompt p
     var = gets.chomp
-      if number?(var) == nil
-        prompt 'Invalid choice. Please try again.'
-      else
-        return var
-        break
-      end
+    if number?(var)
+      return var
+    else
+      prompt 'Invalid choice. Please try again.'
+    end
   end
 end
 
@@ -56,15 +56,14 @@ end
 def calculate(operator, number_1, number_2)
   case operator
   when '1'
-    result = number_1 + number_2
+    number_1 + number_2
   when '2'
-    result = number_1 - number_2
+    number_1 - number_2
   when '3'
-    result = number_1 * number_2
+    number_1 * number_2
   when '4'
-    result = number_1.to_f / number_2.to_f
+    number_1.to_f / number_2.to_f
   end
-  return result
 end
 
 # displays selected operator
@@ -86,7 +85,7 @@ end
 ###################
 
 # collecting user name
-name = get_name()
+name = collect_name
 
 # welcome prompt
 prompt "Hello #{name}! Lets tackle those calculations now."
@@ -94,8 +93,8 @@ prompt "Hello #{name}! Lets tackle those calculations now."
 # main loop
 loop do
   # prompting user number input
-  number_1 = get_user_input('Please enter the first number:').to_f
-  number_2 = get_user_input('Please enter the second number:').to_f
+  number_1 = user_input('Please enter the first number:').to_f
+  number_2 = user_input('Please enter the second number:').to_f
 
   # prompt for operator
   operator_prompt = <<-MSG
@@ -110,7 +109,7 @@ loop do
   # prompting for user operator
   operator = ''
   loop do
-    operator = get_user_input("Please select the operator 1-4:")
+    operator = user_input("Please select the operator 1-4:")
     if operator.to_i >= 1 && operator.to_i <= 4
       break
     else
@@ -129,7 +128,7 @@ loop do
   prompt "Woah... that was an amazing calculation you did #{name}. Type Y to perform another."
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
-end 
+end
 # end main loop
 
 # good bye prompt
